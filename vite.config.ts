1fc01a59379407d78b5fbeecbe9aa461b4f3c1df
VERISON TRAVEL <verison.2023@gmail.com>
2025-10-06 23:29:21 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: isNetlify ? '/' : '/RESERVATION-SOKHNA-CARTE/', // ✅ Auto-switch
+  base: '/', // ✅ for Netlify or any custom domain
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
